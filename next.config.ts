a9b3e41bf72a26af185113491e1affb81881d76a
MNamed2006 <moh2300263@gmail.com>
2026-04-14 11:53:28 +0000

    deploy static export on main

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',        // يخلي المشروع يتولد كـ static
-  images: {
-    unoptimized: true,     // عشان مشاكل الصور في GitHub Pages
-  },
-  basePath: '/jaraha-wai', // مهم جدًا عشان اللينك يشتغل صح على GitHub Pages
+  output: 'export',
+  basePath: '/jaraha-wai',
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
+
